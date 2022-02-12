7553404de45f0917461e572970ea109e1e4546ff
Tosin Shada <shardoeski@gmail.com>
2022-02-12 13:22:26 +0100

    commit

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,6 +1,7 @@
 const constants = {
     // HOST_URL: process.env.REACT_APP_HOST_URL || 'http://localhost:5000',
-    HOST_URL: process.env.REACT_APP_HOST_URL || 'https://anony-vote-backend.vercel.app',
+    //HOST_URL: process.env.REACT_APP_HOST_URL || 'https://anony-vote-backend.vercel.app',
+    HOST_URL: process.env.REACT_APP_HOST_URL || 'https://anonyvote.herokuapp.com',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json'
